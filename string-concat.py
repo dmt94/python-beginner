0bc65b8a5f70423a756758d27fbb2cc22c70f17d
Daevah Taylaran <daevtayl@gmail.com>
2022-08-15 11:38:57 -0700

    ch2 strings

---
+++ string-concat.py
@@ -0,0 +1,9 @@
+first_name = 'daevah'
+middle_initial = 'm'
+last_name = 'taylaran'
+
+# f-strings : f = format
+# python formats the string by replacing each variable within {} with its value
+
+full_name = f"{first_name} {middle_initial}arie {last_name}".title()
+print(full_name) #Daevah Marie Taylaran
