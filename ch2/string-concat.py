ec4db92d6640c3210fa2576965410e3c32054107
Daevah Taylaran <daevtayl@gmail.com>
2022-08-15 12:04:12 -0700

    added folder

---
+++ ch2/string-concat.py
@@ -0,0 +1,9 @@
+first_name = 'daevah'
+middle_initial = 'm'
+last_name = 'taylaran'
+
+# f-strings : f = format , Python 3.6 
+# python formats the string by replacing each variable within {} with its value
+
+full_name = f"{first_name} {middle_initial}aries {last_name}".title()
+print(full_name) #Daevah Maries Taylaran
---
+++ ch2/string-spaces.py
@@ -0,0 +1,12 @@
+# tabs 
+with_tabs = "\tWith tab"
+print(with_tabs)
+#returns:     With tab
+
+language1 = 'javaScript'
+language2 = 'python'
+language3 = 'java'
+
+intro_my_lang = "Languages I am currently learning:"
+my_languages = f"\n{language1}\n{language2}\n{language3}".title()
+print(intro_my_lang + my_languages)
