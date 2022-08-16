b5768eb62d37e448202fd7952fbf4d83d3340c61
Daevah Taylaran <daevtayl@gmail.com>
2022-08-15 19:23:02 -0700

    python done

--- ch2/string-spaces.py
+++ ch2/string-spaces.py
@@ -3,10 +3,13 @@ with_tabs = "\tWith tab"
 print(with_tabs)
 #returns:     With tab
 
-language1 = 'javaScript'
-language2 = 'python'
-language3 = 'java'
+language1 = '\tjavaScript'
+language2 = '\tpython'
+language3 = '\tjava'
 
 intro_my_lang = "Languages I am currently learning:"
 my_languages = f"\n{language1}\n{language2}\n{language3}".title()
 print(intro_my_lang + my_languages)
+
+#  Python makes it easy to eliminate extraneous whitespace from data that people enter.
+
